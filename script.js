d9a7e585ad7f0ff257a205324f0c355e3ebec6b1
Heng Sokkheng <hengsokkheng145@gmail.com>
2024-03-21 08:20:57 +0700

    complete

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
 const generateForm = document.querySelector(".generator-form");
 
-const imageGallery =document.querySelector('.image-gallery');
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "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";
+let isImageGerating = false;
+
+const updateImg = (imgArray) =>{
+  imgArray.forEach((imgObject, index)=>{
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const dLbtn = imgCard.querySelector(".download-btn");
+
+    //Set the img source to the ai generated img dat
+    const aiGenerateImg = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src =  aiGenerateImg;
+
+    //When the image is load, remove load class
+    imgElement.onload=()=> {
+      imgCard.classList.remove('loading');
+      dLbtn.setAttribute("href", aiGenerateImg);
+      dLbtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    }
+  })
+}
+
+const generateAiImage = async (userPrompt, userImgQuantity) => {
+  try {
+    const respone = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt: userPrompt,
+          n: parseInt(userImgQuantity),
+          size: "512x512",
+          response_format: "b64_json"
+        }),
+      }
+    );
+    if(!respone.ok){throw new Error("Server error")};
+    const {data} = await respone.json(); //Get data from the respone
+    updateImg([...data]);
+  } catch (error) {
+    alert(error.message);
+  }finally{
+    isImageGerating = false;
+  }
+};
 
 const handleFormSubmission = (e) => {
   e.preventDefault();
+  if(isImageGerating) return;
+  isImageGerating=true;
 
   //Get user Input and Image quantity values from the form
 
@@ -25,7 +77,8 @@ const handleFormSubmission = (e) => {
     `
   ).join("");
 
- imageGallery.innerHTML = imgCardMarkup;
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImage(userPrompt, userImgQuantity);
 };
 
 generateForm.addEventListener("submit", handleFormSubmission);
